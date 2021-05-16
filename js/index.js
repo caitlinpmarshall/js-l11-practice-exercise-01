0424e50be9c749a2a91da48de22792da9b360aba
caitlinpmarshall <caitlinpmarshall@gmail.com>
2021-05-16 15:45:08 -0400

    random user generator built with JS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function(){
+    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+    const data = await usersRequest.json();
+    const userResults = data.results;
+    displayUsers(userResults);
+    console.log(userResults);
+};
+
+getData();
+
+const displayUsers = function(userResults) {
+    randomFolks.innerHTML = ""; //clears randomFolks element; don't quite understand this step
+
+    for (const user of userResults){ //why const, and not let?
+        const country = user.location.country;
+        const name = user.name.first;
+        const pic = user.picture.medium; //didn't like imageUrl for some reason (typo?)
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+            <h3>${name}</h3>
+            <p>${country}</p>
+            <img src=${pic} alt="user avatar" />
+            `;
+        randomFolks.append(userDiv);
+    }
+};
