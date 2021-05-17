734bb1cd8d695421d20c3c51af05a134a43eb173
caitlinpmarshall <caitlinpmarshall@gmail.com>
2021-05-16 20:30:52 -0400

    Reworked same code for practice; JS creates a random user when page refreshes

--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
 const randomFolks = document.querySelector(".random-peeps");
 const selectUserNumber = document.querySelector("#users");
 
-const getData = async function(numUsers){
+const getData = async function(numUsers){//why ok to use same variable as parameter and argument?
     const usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`);
     const data = await usersRequest.json();
     const userResults = data.results;
+    console.log(userResults);
     displayUsers(userResults);
 };
 
 getData(1);
 
 const displayUsers = function(userResults) {
-    randomFolks.innerHTML = ""; //clears randomFolks element; don't quite understand this step
-
-    for (const user of userResults){ //why const, and not let?
+    randomFolks.innerHTML = ""; //how do you know when this is needed?
+    for (const user of userResults) {
         const country = user.location.country;
         const name = user.name.first;
-        const pic = user.picture.medium; //didn't like imageUrl for some reason (typo?)
+        const pic = user.picture.medium;
         const userDiv = document.createElement("div");
         userDiv.innerHTML = `
-            <h3>${name}</h3>
-            <p>${country}</p>
-            <img src=${pic} alt="user avatar" />
-            `;
+        <h3>${name}</h3>
+        <p>${country}</p>
+        <img src=${pic} alt="user avatar"/>
+        `;
         randomFolks.append(userDiv);
     }
 };
